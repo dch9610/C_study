85e5652c8b4b7256f12ad758fe20a460a54934c3
dch9610 <dch9610@naver.com>
2022-02-25 15:28:36 +0900

    클래스 템플릿

--- Project6/template.cpp
+++ Project6/template.cpp
@@ -3,28 +3,23 @@
 
 using namespace std;
 
-
+// 데이터 클래스 템플릿
 template <typename T>
-void change(T& a, T& b) {
-	T temp;
-	temp = a;
-	a = b;
-	b = temp;
-}
-
-// 명시적 특수화 
-// 다른 데이터 타입일 경우
-template<> void change<int>(int& a, int& b) {
-	cout << "정수형 데이터를 교체합니다.\n";
-	int temp;
-	temp = a;
-	a = b;
-	b = temp;
-}
+class Data {
+private:
+	T data;
+public:
+	Data(T data) : data(data) { }
+	void setData(T data) {
+		this->data = data;
+	}
+	T getData{
+		return data;
+	}
+};
 
 int main(void) {
-	int a = 4;
-	int b = 8;
-	change(a, b);
-	cout << a << ":" << b << endl;
+	Data<int> data1(2);
+	Data<string> data2("이순신");
+	cout << data1.getData() << ":" << data2.getData() << "\n";
 }
