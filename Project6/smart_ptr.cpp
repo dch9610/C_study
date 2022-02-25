f7096996b8ae3da0ebbf126dc0ef5411941d59d0
dch9610 <dch9610@naver.com>
2022-02-25 18:28:37 +0900

    weak_ptr

--- Project6/smart_ptr.cpp
+++ Project6/smart_ptr.cpp
@@ -3,18 +3,17 @@
 using namespace std;
 
 int main(void) {
-	int* arr = new int[10];
-	arr[7] = 100;
-	shared_ptr<int> p1(arr);
-	cout << p1.use_count() << "\n";
-	shared_ptr<int> p2(p1);
-	cout << p1.use_count() << "\n";
-	shared_ptr<int> p3 = p2;
-	cout << p1.use_count() << "\n";
-	p1.reset();
-	p2.reset();
-	cout << "arr[7]: " << arr[7] << "\n";
-	p3.reset();
-	cout << p1.use_count() << "\n";
-	cout << "arr[7]: " << arr[7] << "\n";
+	int* arr = new int(1);
+	shared_ptr<int> sp1(arr);
+	weak_ptr<int> wp = sp1;
+	cout << sp1.use_count() << "\n";
+	cout << wp.use_count() << "\n";
+	if (true) {
+		shared_ptr<int> sp2 = wp.lock(); // shared_ptr 포인터 변환
+		cout << sp1.use_count() << "\n";
+		cout << wp.use_count() << "\n";
+	}
+	// 스코프를 벗어나므로 sp2가 해제됩니다.
+	cout << sp1.use_count() << "\n";
+	cout << wp.use_count() << "\n";
 }
