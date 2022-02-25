9c4447e2bc10febc15944c58876fe555135aaeca
dch9610 <dch9610@naver.com>
2022-02-25 14:41:39 +0900

    순수 가상 함수

--- Project6/polymorphsim.cpp
+++ Project6/polymorphsim.cpp
@@ -4,24 +4,25 @@ using namespace std;
 
 class A {
 public:
-	// 가상함수 virtual 사용
-	virtual void show() {
+	// 순수 가상 함수 '=0' 사용
+	virtual void show()=0 {
 		cout << "A클래스 입니다" << endl;
 	}
 };
 
 class B :public A {
+	// 순수 가상 함수를 사용하여서
+	// show() 함수를 재정의하지 않으면 B 클래스의 객체를 사용할 수 없음
+public:
 	virtual void show() {
 		cout << "B클래스 입니다" << endl;
 	}
+
 };
 
 int main(void) {
 	A* p;
-	A a;
 	B b;
-	p = &a;
-	p->show();
 	p = &b;
 	p->show(); 
 }
