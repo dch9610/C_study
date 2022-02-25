74fec31b891921b3b89ffcee32cda49a1b7d35f7
dch9610 <dch9610@naver.com>
2022-02-25 18:28:11 +0900

    shared_ptr

--- Project6/smart_ptr.cpp
+++ Project6/smart_ptr.cpp
@@ -3,8 +3,18 @@
 using namespace std;
 
 int main(void) {
-	unique_ptr<int> p1(new int(19));
-	cout << *p1 << "\n"; // 관리하고 있는 객체를 반환
-
-
+	int* arr = new int[10];
+	arr[7] = 100;
+	shared_ptr<int> p1(arr);
+	cout << p1.use_count() << "\n";
+	shared_ptr<int> p2(p1);
+	cout << p1.use_count() << "\n";
+	shared_ptr<int> p3 = p2;
+	cout << p1.use_count() << "\n";
+	p1.reset();
+	p2.reset();
+	cout << "arr[7]: " << arr[7] << "\n";
+	p3.reset();
+	cout << p1.use_count() << "\n";
+	cout << "arr[7]: " << arr[7] << "\n";
 }
