4616169c84b02125387f10ec94956bfc2f4273f1
dch9610 <dch9610@naver.com>
2022-02-25 18:06:48 +0900

    unique_ptr : 메모리 해제 이후에 객체에 접근하기

--- Project6/template.cpp
+++ Project6/template.cpp
@@ -1,25 +1,25 @@
-#include <iostream>
-#include <string>
-
-using namespace std;
-
-// 데이터 클래스 템플릿
-template <typename T=int>
-class Data {
-private:
-	T data;
-public:
-	Data(T data) : data(data) { }
-	void setData(T data) {
-		this->data = data;
-	}
-	T getData{
-		return data;
-	}
-};
-
-int main(void) {
-	Data<> data1(2);
-	Data<string> data2("이순신");
-	cout << data1.getData() << ":" << data2.getData() << "\n";
-}
+//#include <iostream>
+//#include <string>
+//
+//using namespace std;
+//
+//// 데이터 클래스 템플릿
+//template <typename T=int>
+//class Data {
+//private:
+//	T data;
+//public:
+//	Data(T data) : data(data) { }
+//	void setData(T data) {
+//		this->data = data;
+//	}
+//	T getData{
+//		return data;
+//	}
+//};
+//
+//int main(void) {
+//	Data<> data1(2);
+//	Data<string> data2("이순신");
+//	cout << data1.getData() << ":" << data2.getData() << "\n";
+//}
