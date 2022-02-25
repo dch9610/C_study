d5476f385455c6dcf58eb308dbb95287560a275a
dch9610 <dch9610@naver.com>
2022-02-25 14:58:24 +0900

    명시적 특수화

--- Project6/template.cpp
+++ Project6/template.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
+
 template <typename T>
 void change(T& a, T& b) {
 	T temp;
@@ -11,9 +12,19 @@ void change(T& a, T& b) {
 	b = temp;
 }
 
+// 명시적 특수화 
+// 다른 데이터 타입일 경우
+template<> void change<int>(int& a, int& b) {
+	cout << "정수형 데이터를 교체합니다.\n";
+	int temp;
+	temp = a;
+	a = b;
+	b = temp;
+}
+
 int main(void) {
-	string a = "홍길동";
-	string b = "이순신";
+	int a = 4;
+	int b = 8;
 	change(a, b);
 	cout << a << ":" << b << endl;
 }
