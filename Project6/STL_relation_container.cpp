67ac39c4c77332faeba28ea3e9c7eecbcfb50e50
dch9610 <dch9610@naver.com>
2022-02-28 12:00:40 +0900

    STL 연관 컨테이너 : 집합

--- Project6/STL_relation_container.cpp
+++ Project6/STL_relation_container.cpp
@@ -8,7 +8,7 @@ int main(void) {
 	map<string, int> m;
 	m["홍길동"] = 1; m["이순신"] = 2; m["세종대왕"] = 3;
 	map<string, int>::iterator iter;
-	iter= m.begin();
+	iter = m.begin();
 	for (; iter != m.end(); iter++) {
 		// iter->first : key의 값만 출력
 		// iter->second : value의 값만 출력
@@ -16,7 +16,7 @@ int main(void) {
 		cout << iter->first << ":" << iter->second << "\n";
 	}
 	cout << "\n";
-	
+
 	m["임꺽정"] = 4;
 	// 없는 데이터면 0이 출력
 	cout << m["없는사람"] << "\n";
