b56df67ded8118a7fec364e5419d42a5f005d011
dch9610 <dch9610@naver.com>
2022-02-28 14:01:06 +0900

    클래스에서의 예외처리

--- Project6/exception.cpp
+++ Project6/exception.cpp
@@ -2,16 +2,38 @@
 
 using namespace std;
 
-int main(void) {
-	int a = 7, b = 0;
-	try {
-		if (b == 0) {
-			throw "0으로 나눌 수 없습니다.";
-		}
-		cout << a / b << "\n";
+template <typename T>
+class Data {
+private:
+	T data;
+public:
+	// 
+	Data(T data) : data(data){ }
+	T getData() {
+		return data;
 	}
-	// 예외가 발생했을 시에 throw를 받아서 출력함
-	catch (const char* str) {
-		cout << str << "\n";
+	Data<T> operator /(const Data<T>& other) {
+		if (other.data == 0) {
+			throw 0;
+		}
+		return Data(data / other.data);
+	}
+
+};
+
+
+
+int main(void) {
+	// a에 Data Class를 담음
+	try {
+		Data<int> a(7);
+		Data<int> b(0);
+		Data<int> result = a / b;
+		cout << result.getData() << "\n";
+	}
+	catch (int e) {
+		if (e == 0) {
+			cout << "0으로 나눌 수 없습니다.\n";
+		}
 	}
 }
