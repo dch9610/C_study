03f01e1a0658bf78c5a18db0dfedf12c5695ef8b
dch9610 <dch9610@naver.com>
2022-02-28 11:07:48 +0900

    STL 컨테이너 어댑터 : 우선순위 큐

--- Project6/STL_container_adapter.cpp
+++ Project6/STL_container_adapter.cpp
@@ -3,11 +3,19 @@
 
 using namespace std;
 
+// 큰수대로 출력 (우선순위 큐를 사용)
 int main(void) {
-	queue<int> s;
-	s.push(7); s.push(5); s.push(4); s.pop(); s.push(6); s.pop();
-	while (!s.empty()) {
-		cout << s.front() << " ";
-		s.pop();
+	int n=0, x;
+	cin >> n;
+	priority_queue<int> pq;
+	for (int i = 0; i < n; i++) {
+		cin >> x;
+		pq.push(x);
 	}
+
+	while (!pq.empty()) {
+		cout << pq.top() << ' ';
+		pq.pop();
+	}
+	
 }
