2c8a8f865d808a3668b102a8844d3bbafb2c7af7
dch9610 <dch9610@naver.com>
2022-03-14 20:59:22 +0900

    smart pointer 사용

--- C++ Class/main.cpp
+++ C++ Class/main.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
+#include <memory>
 using namespace std;
 
+void cal(int *a, int *b){
+    int sum = (*a) + (*b);
+    int sub = (*a) - (*b);
+    *a = sum;
+    *b = sub;
+}
+
 int main(){
-    int SIZE=0;
-    cin >> SIZE;
-    int *arr = new int[SIZE];
+    unique_ptr<int> a = unique_ptr<int> {new int{}};
+    unique_ptr<int> b {new int {}};
 
-    for(int i=0; i<SIZE; i++){
-        cin >> arr[i];
-    }
-
-    int SEARCH = 0;
-    cin >> SEARCH;
-    cout << arr[arr[SEARCH]];
-    delete [] arr;
+    cin >> *a >> *b;
+    cal(a.get(),b.get());
+    cout << *a << endl;
+    cout << *b << endl;
 }
