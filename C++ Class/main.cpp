43dc801d959ad8fdc468cf55d55a5a3de7437218
dch9610 <dch9610@naver.com>
2022-03-14 21:00:39 +0900

    실습과제1_types

--- C++ Class/main.cpp
+++ C++ Class/main.cpp
@@ -1,27 +1,62 @@
 #include <iostream>
-#include <memory>
+#include <limits>
 #include <string>
-#include <tuple>
+#include <algorithm>
 using namespace std;
 
-typedef tuple <string, int> tuple1;
-
-auto sum(int a, int b){
-    return tuple1{"sum: ",a+b};
+int int_limit(){
+    cout << numeric_limits<int>::max() << " " << numeric_limits<int>::min() << "\n";
 }
 
-int main() {
-    auto pa = make_unique<int>(1);
-    auto pb = make_unique<int>(2);
+long long_limit(){
+    cout << numeric_limits<long long>::max() << " " << numeric_limits<long long>::min() << "\n";
+}
 
-    cout << *pa << ", " << *pb << endl;
-    auto my_swap = [](auto a, auto b) {
-        auto temp = *a;
-        *a = *b;
-        *b = temp;
-    };
-    my_swap(pa.get(), pb.get());
-    cout << "a:" << *pa << ",b:" << *pb << endl;
-    auto [label, result] = sum(*pa, *pb);
-    cout << label << result << endl;
+float float_limit(){
+    cout << numeric_limits<float>::max() << " " << numeric_limits<float>::min() << "\n";
+}
+
+double double_limit(){
+    cout << numeric_limits<double>::max() << " " << numeric_limits<double>::min() << "\n";
+}
+
+int main(){
+    string type_name;
+    int int_count = 0, long_count=0, float_count=0, double_count=0;
+    cin >> type_name;
+    while(type_name != "quit"){
+        transform(begin(type_name), end(type_name), begin(type_name),[](unsigned char c){
+            return tolower(c);
+        });
+
+        if(type_name == "int"){
+            int_limit();
+            int_count += 1;
+            cin >> type_name;
+        }
+        else if(type_name=="long"){
+            long_limit();
+            long_count += 1;
+            cin >> type_name;
+        }
+        else if(type_name=="float"){
+            float_limit();
+            float_count += 1;
+            cin >> type_name;
+        }
+        else if(type_name=="double"){
+            double_limit();
+            double_count += 1;
+            cin >> type_name;
+        }
+        else{
+            cout << "vector" << "\n";
+            cout << "Invalid Command" << "\n";
+            cin >> type_name;
+        }
+
+    }
+    cout << "=== the number of types ===" << "\n";
+    cout << "int: " << int_count << "\n" << "long: " << long_count << "\n";
+    cout << "float: " << float_count << "\n" << "double: " << double_count <<endl;
 }
