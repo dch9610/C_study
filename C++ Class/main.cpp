5187441bc4b2049fcccc04a272ff38765a516b1f
dch9610 <dch9610@naver.com>
2022-03-15 11:22:53 +0900

    포인터의 동적 배열

--- C++ Class/main.cpp
+++ C++ Class/main.cpp
@@ -4,14 +4,21 @@ const int ROW_SIZE=3;
 const int COLUMN_SIZE = 5;
 
 int main(){
-    int intArrayA[ROW_SIZE][COLUMN_SIZE];
-    for (int i =0; i<ROW_SIZE; i++) {
-        for(int j=0; j<COLUMN_SIZE; j++){
-            intArrayA[i][j] = i+j;
+    int intArray1[2] = {0,1};
+    int intArray2[2] = {2,3};
+    int intArray3[2] = {4,5};
+
+    // 3개의 int* 즉 int 배열을 원소로 하는 배열
+    int** const pplntArray = new int*[3];
+    pplntArray[0] = intArray1; // 배열의 이름은 포인터로서 사용됨
+    pplntArray[1] = intArray2;
+    pplntArray[2] = intArray3;
+
+    for(int i=0; i<3; i++){
+        for(int j=0; j<2; j++){
+            cout << pplntArray[i][j] << "\t";
+            cout << endl;
         }
     }
-
-    // 인덱스를 이용한 접근
-    cout << intArrayA[0][2] << " " << intArrayA[1][2] << "\n";
-    cout << *(intArrayA+2) << " " << *(intArrayA+(COLUMN_SIZE*1)+2) << "\n";
+    delete [] pplntArray;
 }
