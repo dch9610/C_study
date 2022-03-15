4520aa4b340b449f774ce5ab1d6d1fc509f6912c
dch9610 <dch9610@naver.com>
2022-03-15 11:15:12 +0900

    2차원 배열

--- C++ Class/main.cpp
+++ C++ Class/main.cpp
@@ -1,19 +1,17 @@
 #include <iostream>
 using namespace std;
-const int SIZE=3;
+const int ROW_SIZE=3;
+const int COLUMN_SIZE = 5;
 
 int main(){
-    // 배열 정렬 초기화
-    // 지정되지 않은 원소의 초기값은 0으로 결정
-    int intArray[SIZE] = {10, 20,30};
-    int sum = 0;
-    for(int i = 0; i<SIZE; i++){
-        sum += intArray[i]; // 원소의 접근
+    int intArrayA[ROW_SIZE][COLUMN_SIZE];
+    for (int i =0; i<ROW_SIZE; i++) {
+        for(int j=0; j<COLUMN_SIZE; j++){
+            intArrayA[i][j] = i+j;
+        }
     }
-    // 문자의 배열은 문자열 리터럴을 이용하여 초기화
-    char strArray1[]="a string"; // {'a', '', 's', 't', 'r', 'i'..}와 동일
 
-    // 포인터를 사용해서도 지정가능
-    cout << *intArray << "\n" << *(intArray +1) << "\n";
-    cout << sum << endl;
+    // 인덱스를 이용한 접근
+    cout << intArrayA[0][2] << " " << intArrayA[1][2] << "\n";
+    cout << *(intArrayA+2) << " " << *(intArrayA+(COLUMN_SIZE*1)+2) << "\n";
 }
