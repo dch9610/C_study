d2de697c26a8516a78460065df6bd4a18204beaa
dch9610 <dch9610@naver.com>
2022-03-15 11:06:00 +0900

    배열 정의

--- C++ Class/main.cpp
+++ C++ Class/main.cpp
@@ -1,62 +1,19 @@
 #include <iostream>
-#include <limits>
-#include <string>
-#include <algorithm>
 using namespace std;
-
-int int_limit(){
-    cout << numeric_limits<int>::max() << " " << numeric_limits<int>::min() << "\n";
-}
-
-long long_limit(){
-    cout << numeric_limits<long long>::max() << " " << numeric_limits<long long>::min() << "\n";
-}
-
-float float_limit(){
-    cout << numeric_limits<float>::max() << " " << numeric_limits<float>::min() << "\n";
-}
-
-double double_limit(){
-    cout << numeric_limits<double>::max() << " " << numeric_limits<double>::min() << "\n";
-}
+const int SIZE=3;
 
 int main(){
-    string type_name;
-    int int_count = 0, long_count=0, float_count=0, double_count=0;
-    cin >> type_name;
-    while(type_name != "quit"){
-        transform(begin(type_name), end(type_name), begin(type_name),[](unsigned char c){
-            return tolower(c);
-        });
-
-        if(type_name == "int"){
-            int_limit();
-            int_count += 1;
-            cin >> type_name;
-        }
-        else if(type_name=="long"){
-            long_limit();
-            long_count += 1;
-            cin >> type_name;
-        }
-        else if(type_name=="float"){
-            float_limit();
-            float_count += 1;
-            cin >> type_name;
-        }
-        else if(type_name=="double"){
-            double_limit();
-            double_count += 1;
-            cin >> type_name;
-        }
-        else{
-            cout << "vector" << "\n";
-            cout << "Invalid Command" << "\n";
-            cin >> type_name;
-        }
-
+    // 배열 정렬 초기화
+    // 지정되지 않은 원소의 초기값은 0으로 결정
+    int intArray[SIZE] = {10, 20,30};
+    int sum = 0;
+    for(int i = 0; i<SIZE; i++){
+        sum += intArray[i]; // 원소의 접근
     }
-    cout << "=== the number of types ===" << "\n";
-    cout << "int: " << int_count << "\n" << "long: " << long_count << "\n";
-    cout << "float: " << float_count << "\n" << "double: " << double_count <<endl;
+    // 문자의 배열은 문자열 리터럴을 이용하여 초기화
+    char strArray1[]="a string"; // {'a', '', 's', 't', 'r', 'i'..}와 동일
+
+    // 포인터를 사용해서도 지정가능
+    cout << *intArray << "\n" << *(intArray +1) << "\n";
+    cout << sum << endl;
 }
