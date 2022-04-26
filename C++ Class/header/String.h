992489c9dbf8e8b3fdfcfce85a8b6f1bd778d553
dch9610 <dch9610@naver.com>
2022-04-26 21:13:42 +0900

    22.04.25 수업 및 과제

---
+++ C++ Class/header/String.h
@@ -0,0 +1,8 @@
+//
+// Created by Lee on 2022-04-26.
+//
+
+#ifndef C___CLASS_STRING_H
+#define C___CLASS_STRING_H
+
+#endif //C___CLASS_STRING_H
--- C++ Class/pointer.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Lee on 2022-03-14.
-//
-
-#ifndef C___CLASS_POINTER_H
-#define C___CLASS_POINTER_H
-
-#endif //C___CLASS_POINTER_H
